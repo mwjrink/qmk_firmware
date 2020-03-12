ffa889261f18562bb921679455c8389d952ade9c
MaxRink <mwjrink@github.com>
2020-03-12 10:08:14 -0400

    v3

--- keyboards/keebio/maximus/config.h
+++ keyboards/keebio/maximus/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x0001
 #define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0002
+#define DEVICE_VER      0x0003
 #define MANUFACTURER    Maximus
 #define PRODUCT         EndGame
 #define DESCRIPTION     My end game keyboard 
